76ce23bffad5c87b42eadf398d5a14c4c6c7504b
Hugo Roque <hugolnx@gmail.com>
2017-06-09 18:31:23 -0300

    Update changelog

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Ravenex.Mixfile do
   def project do
     [
       app: :ravenex,
-      version: "0.0.7",
+      version: "0.0.8",
       elixir: "~> 1.0",
       description: """
         Ravenex is an Elixir client for Sentry. Automatically
